c70de8674fda099af1204278e75dbea2ac08b134
Philippe Matias <96602120+philmts128@users.noreply.github.com>
2025-01-27 15:14:03 -0300

    Update main.cpp

--- queue/main.cpp
+++ queue/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
-
 using namespace std;
+
+#include "queue.h"
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    pm::queue<int> nums;
+
+    for (int i = 1; i <= 10; ++i)
+        nums.enqueue(i);
+
+    while (!nums.empty())
+        cout << nums.dequeue() << " ";
+
     return 0;
 }
