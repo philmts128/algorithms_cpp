3a72ef8099da57a452089e7ed8496c15632f1071
Philippe Matias <96602120+philmts128@users.noreply.github.com>
2025-01-28 14:43:33 -0300

    Update stack.h

--- stack_node_based/stack.h
+++ stack_node_based/stack.h
@@ -1,6 +1,7 @@
 #ifndef STACK_H
 #define STACK_H
 
+#include <exception>
 #include "stack_node.h"
 
 
@@ -11,12 +12,56 @@ namespace pm
     class stack
     {
     private:
+        /*----------------------*/
         stack_node<T>* m_top { nullptr };
+        stack_node<T>* m_first { nullptr };
+        int m_size { 0 };
 
     public:
+        /*----------------------*/
         stack() {}
+        ~stack() { if (m_top) delete m_top; }
+
+        /*----------------------*/
+        void push(const T& data)
+        {
+            if (this->empty()) {
+                m_top = new stack_node<T>(data);
+                m_first = m_top;
+                m_size++;
+                return;
+            }
+
+            m_top->next = new stack_node<T>(data);
+            m_top = m_top->next;
+            m_size++;
+        }
+
+        /*----------------------*/
+        auto pop() -> const T&
+        {
+            auto temp = m_top;
+            auto data = temp->data;
+
+            stack_node<T>* cur = m_first;
+            while (cur->next != nullptr)
+                cur = cur->next;
+
+            m_top = cur;
+            m_top->next = nullptr;
+
+            delete temp;
+            m_size--;
+
+            return data;
+        }
+
+        /*----------------------*/
+        auto top() -> const T& { return m_top->data; }
+        int size() const { return m_size; }
+        bool empty() const { return (m_size == 0); }
     };
 }
 
 
-#endif // STACK_H_INCLUDED
+#endif
