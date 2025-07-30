5f2a3a36c1ec8ddd01ffd45250cb97f29d035fcb
philippematias <felipmatias88@gmail.com>
2025-07-29 23:42:16 -0300

    mais algumas funcionalidades no vetor

--- src/02_vector/vector/vector.h
+++ src/02_vector/vector/vector.h
@@ -31,6 +31,29 @@ namespace pm
         }
 
         /*----------------------------------*/
+        vector(size_t size, bool init = true) {
+            this->m_size = size;
+            this->reserve(calc_capacity(size));
+
+            if (init) {
+                for (size_t i = 0; i < m_size; ++i) {
+                    m_data[i] = T{};
+                }
+            }
+        }
+
+        /*----------------------------------*/
+        template <typename... Args>
+        vector(const Args&... args)
+        {
+            size_t len = sizeof...(args);
+            this->m_size = len;
+            m_capacity = calc_capacity(len);
+            if (m_data) delete[] m_data;
+            m_data = new T[m_capacity] { static_cast<T>(args)... };
+        }
+
+        /*----------------------------------*/
         vector(const pm::vector<T>& vec) {
             this->copy_data(vec);
         }
@@ -120,7 +143,7 @@ namespace pm
         }
 
         /*----------------------------------*/
-        bool exists(const T& value) const {
+        bool exists(const T& value) const { //O(n)
             return this->search(value) != Not_Found;
         }
 
@@ -159,13 +182,42 @@ namespace pm
         }
 
         /*----------------------------------*/
-        bool operator != (const pm::vector<T>& vec) const {
+        bool operator != (const pm::vector<T>& vec) const { //O(n)
             return !(*this == vec);
         }
 
         /*----------------------------------*/
         auto operator [] (const size_t index) -> T& { //O(1)
             return m_data[index];
+        }
+
+        /*----------------------------------*/
+        auto operator + (const pm::vector<T>& other) -> const pm::vector<T>
+        {
+            pm::vector<T> vec(this->m_size + other.m_size, false);
+
+            size_t i = 0;
+            for (; i < this->m_size; ++i)
+                vec.m_data[i] = this->m_data[i];
+
+            for (size_t j = 0; j < other.m_size; ++j)
+                vec.m_data[i+j] = other.m_data[j];
+
+            return vec;
+        }
+
+        /*----------------------------------*/
+        auto operator += (const pm::vector<T>& other) -> pm::vector<T>&
+        {
+            size_t i = m_size;
+
+            m_size += other.m_size;
+            this->reserve(m_size);
+
+            for (size_t j = 0; j < other.m_size;)
+                m_data[i++] = other.m_data[j++];
+
+            return *this;
         }
 
         /*----------------------------------*/
@@ -182,7 +234,7 @@ namespace pm
         }
 
         /*----------------------------------*/
-        void left_shift(size_t start_position = 0) {
+        void left_shift(size_t start_position = 0) { //O(n-s)
             auto sp = start_position;
             for (; sp < m_size-1; ++sp) {
                 m_data[sp] = m_data[sp+1];
@@ -190,7 +242,7 @@ namespace pm
         }
 
         /*----------------------------------*/
-        void reserve(size_t capacity)
+        void reserve(size_t capacity) //O(min(capacity, m_size))
         {
             if (m_size == capacity)
                 return;
@@ -222,8 +274,7 @@ namespace pm
         }
 
         /*----------------------------------*/
-        void shrink()
-        {
+        void shrink() {
             m_capacity = this->calc_capacity(m_size);
             this->reserve(m_capacity);
         }
@@ -231,8 +282,8 @@ namespace pm
         /*----------------------------------*/
         void copy_data(const pm::vector<T>& vec) //O(vec.m_size)
         {
-            m_capacity = this->calc_capacity(m_size);
             m_size = vec.m_size;
+            m_capacity = this->calc_capacity(m_size);
 
             if (m_data) delete[] m_data;
             m_data = new T[m_capacity];
